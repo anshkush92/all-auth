86d8dfb83391d6f67706ef38658e210a9e3060f3
anshkush92college <imt_2020013@iiitm.ac.in>
2022-10-07 18:53:56 +0530

    :sparkles: Connection to database made successfully
    Learnt how to connect to the mongodb using the mongoose and callback and use in server.js and make credentials confidential      using dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 // Importing the Express module (library/file)
 const express = require("express");
+// Using the dotenv package for better security
+require("dotenv").config();
 
 // Creating an express application named app
 const app = express();
+
+// Now importing / accessing the database connection
+const db = require("./db/connection");
 
 // Defining the PORT on which our server will be running
 const PORT = 8000 || process.env.PORT;
