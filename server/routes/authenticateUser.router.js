7da1d454190e4979b38c74d3ac0a874a7fe850b6
anshkush92college <imt_2020013@iiitm.ac.in>
2022-10-17 17:44:26 +0530

    :bug: Created the authenticate middleware to authenticate the user on the backend

--- server/routes/authenticateUser.router.js
+++ server/routes/authenticateUser.router.js
@@ -1,11 +1,28 @@
+// Test -------------- Dependencies for the Route ---------------------------------------------------
 const express = require("express");
+const userSchema = require("../models/user.model");
 const authenticateUserRouter = express.Router();
 const authenticateUserMiddleware = require("../middlewares/autheticateUser.middleware");
 
 // Test --------------- GET /user to check whether user is authenticate or not -----------------------
 authenticateUserRouter.get("/user", authenticateUserMiddleware, async (req, res) => {
     console.log("In authenticate user Router /user");
-    res.status(200).json({ message: "Request to /user to authenticate user" });
+
+    // Again validating the user in this route
+    try {
+        // Checking again whether the user is valid or not 
+        const isValidUser = await userSchema.findOne({ _id: req.userId });
+        console.log("Is Valid User ?", isValidUser);
+
+        if (isValidUser) {
+            res.status(200).json({ message: "The user is Valid", user: isValidUser });
+        } else {
+            res.status(401).json({ message: "The user is not Valid", user: isValidUser });
+        }
+    } catch (error) {
+        res.status(401).json({ message: "Error", error })
+        console.log(error);
+    }
 });
 
 // Test --------------- Exporting the authenicate User Router ---------------------------------------
