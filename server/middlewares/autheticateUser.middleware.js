da7c605fd12e4bc10fd5ed4df796818b8c92e066
anshkush92college <imt_2020013@iiitm.ac.in>
2022-10-17 15:20:22 +0530

    :ambulance: Able to write the logic for authenticating the user in the middlewares

--- server/middlewares/autheticateUser.middleware.js
+++ server/middlewares/autheticateUser.middleware.js
@@ -9,11 +9,32 @@ const jwtSecretKey = process.env.JWT_SECRET_KEY;
 // Since this is a middleware function so we need to use the next() otherwise the control will get stuck on that middleware func only
 const authenticateUser = async (req, res, next) => {
     try {
-        const token = req.headers.Authorization;
-        console.log(token);
+        // Getting the token from the header -----> Make sure to use the "a" instead of "A" in Authorization
+        const token = req.headers.authorization;
+
+        // On verifying the JWT Token, we will get the "Payload" which we used to create the token
+        const verifyJwtToken = jwt.verify(token, jwtSecretKey);
+
+        // On getting the "Payload", now we will "check", whether this "certain id" exsists or not in DB or not
+        const isValidUser = await userSchema.findOne({ _id: verifyJwtToken.id });
+
+        console.log("JWT Secret Key: ", jwtSecretKey, "\nJWT Token for /user: ", token, "\nIs JWT correct ?", verifyJwtToken);
+        console.log("User credentials: ", isValidUser);
+
+        if (!isValidUser) {
+            throw new Error("User not found");
+        }
+
+        // Setting these values, when we have got the user 
+        req.token = token;
+        req.user = isValidUser;
+        req.userId = isValidUser._id;
+
     } catch (error) {
         console.log(error);
+        res.status(401).json({ message: "Unauthorized Access" });
     }
+
     console.log("In Authenticate User Middleware");
     next();
 }
