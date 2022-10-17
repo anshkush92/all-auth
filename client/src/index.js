5d57f67a570df7d39a211704415695038e00f91e
anshkush92college <imt_2020013@iiitm.ac.in>
2022-10-17 18:17:20 +0530

    :art: Added the user Context Provider to the index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom"
 import AuthContextProvider from './app/AuthContext/AuthContextProvider';
+import UserContextProvider from './app/UserContext/UserContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
