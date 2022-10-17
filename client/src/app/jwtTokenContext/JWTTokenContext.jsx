fd6f84052f0a9ad285f645869e21591f6fdb35f6
anshkush92college <imt_2020013@iiitm.ac.in>
2022-10-17 18:00:13 +0530

    :art: Added the JWT Auth Token Context to perfrom CRUD operation with JWT token in the local storage

---
+++ client/src/app/jwtTokenContext/JWTTokenContext.jsx
@@ -0,0 +1,10 @@
+// Test -------------------------- Importing the Packages ---------------------------------
+import { createContext } from "react";
+
+// Test -------------------------- Importing the styles / other components ----------------
+
+// Test -------------------------- The current component ----------------------------------
+const JWTTokenContext = createContext();
+
+// Test -------------------------- Exporting the current component ------------------------
+export default JWTTokenContext;
